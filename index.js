859d86e381a3871d3fb4d53977ab1bd118133456
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-04 19:54:42 +0200

    More testdata

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict'
 
-var getBoards = require('./lib/getBoards')
-var getDetails = require('./lib/getDetails')
-
-module.exports.getBoards = getBoards
-
-module.exports.getMeetings = require('./lib/get-meetings')
-
 module.exports.getAgenda = require('./lib/get-agenda')
 
-module.exports.getDetails = getDetails
+module.exports.getBoards = require('./lib/get-boards')
+
+module.exports.getDetails = require('./lib/get-details')
+
+module.exports.getMeetings = require('./lib/get-meetings')
