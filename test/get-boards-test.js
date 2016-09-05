e875d3a0f7a2d1b2c28438529479b0fa9a9ac477
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-05 10:51:38 +0200

    Updates get-boards. Adds test

---
+++ test/get-boards-test.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const tap = require('tap')
+const getBoards = require('../lib/get-boards')
+
+tap.test('It requires an options object', test => {
+  const options = false
+  const expectedErrorMessage = 'Missing required input: options object'
+  getBoards(options, (error, data) => {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It requires options.host', test => {
+  const options = {
+    host: false
+  }
+  const expectedErrorMessage = 'Missing required input: options.host'
+  getBoards(options, (error, data) => {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It requires options.host to be a valid url', test => {
+  const options = {
+    host: 'pysjepreik'
+  }
+  const expectedErrorMessage = 'Input error: options.host is not a valid url'
+  getBoards(options, (error, data) => {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It requires options.path', test => {
+  const options = {
+    host: 'http://opengov.cloudapp.net',
+    path: false
+  }
+  const expectedErrorMessage = 'Missing required input: options.path'
+  getBoards(options, (error, data) => {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It returns expected results', test => {
+  const options = {
+    host: 'http://opengov.cloudapp.net',
+    path: '/Meetings/tfk'
+  }
+  const expectedResult = require('./data/get-boards-data.json')
+  getBoards(options, (error, data) => {
+    if (error) {
+      throw error
+    } else {
+
+    }
+    tap.equal(JSON.stringify(expectedResult), JSON.stringify(data), 'Returned boards OK')
+    test.done()
+  })
+})
